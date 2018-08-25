9f3c388715d105396f4904bd9c82cb577802cbe5
justi <justi@DESKTOP-O7K8SPS.Home>
2018-08-25 17:01:34 -0400

    ok

--- Person/src/Main.cpp
+++ Person/src/Main.cpp
@@ -9,6 +9,13 @@
 using namespace std;
 #include "Person.h"
 
+Person *createPerson() {
+
+	Person *pnPerson = new Person();
+	pnPerson->setName("Cloud");
+	return pnPerson;
+}
+
 int main() {
 	cout << "Hello" << endl; // prints Hello
 
@@ -44,14 +51,29 @@ int main() {
 
 	cout << p4.toString() << endl;
 
-
 	Person p5 = p1;
 	cout << p5.toString() << endl;
 
-
 	Person p6(p1);
 
-	cout << "HUH?" <<  p6.toString() << endl;
+	cout << "ok" << p6.toString() << endl;
+
+	// new operator must be followed by the delete operator to free object from memory
+
+	Person *pPerson = new Person();
+	pPerson->setName("Lance");
+	pPerson->setAge(22);
+	pPerson->toString();
+
+
+	//create a new object from a function
+
+	Person *pnCloud = new Person();
+	pnCloud->setName("cloud");
+
+	cout << pnCloud->toString() << endl;
+
+	delete pnCloud;
 
 	return 0;
 
